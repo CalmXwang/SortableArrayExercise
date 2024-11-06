51a00e0b96ddce082c0d26c1b8fb667efad609ae
CalmXwang <whz643348767@gmail.com>
2024-11-06 15:53:44 -0800

    Add integer sorting feature and test

--- SortableArrayExercise/src/SortableArrayTest.java
+++ SortableArrayExercise/src/SortableArrayTest.java
@@ -8,18 +8,9 @@ public class SortableArrayTest {
         int[] inputArray = {5, 3, 8, 1, 2};
         int[] expectedArray = {1, 2, 3, 5, 8};
         
+        // 调用排序方法
         array.sortIntegers(inputArray);
         
         assertArrayEquals(expectedArray, inputArray, "The integer array should be sorted in ascending order.");
-    }
-
-    @Test
-    public void testSortStrings() {
-        String[] inputArray = {"banana", "apple", "cherry", "date"};
-        String[] expectedArray = {"apple", "banana", "cherry", "date"};
-        
-        array.sortStrings(inputArray);
-        
-        assertArrayEquals(expectedArray, inputArray, "The string array should be sorted in alphabetical order.");
     }
 }
--- SortableArrayExercise/src/array.java
+++ SortableArrayExercise/src/array.java
@@ -7,18 +7,9 @@ public class array {
         System.out.println("Sorted Integer Array: " + Arrays.toString(array));
     }
 
-    public static void sortStrings(String[] array) {
-        Arrays.sort(array); 
-        System.out.println("Sorted String Array: " + Arrays.toString(array));
-    }
-
     public static void main(String[] args) {
-        int[] intArray = {5, 3, 8, 1, 2};
+        int[] intArray = {6,7,2,1,5};
         System.out.println("Original Integer Array: " + Arrays.toString(intArray));
-        sortIntegers(intArray);
-
-        String[] stringArray = {"banana", "apple", "cherry", "date"};
-        System.out.println("Original String Array: " + Arrays.toString(stringArray));
-        sortStrings(stringArray);
+        sortIntegers(intArray); 
     }
 }
