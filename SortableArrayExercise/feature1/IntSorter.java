db3233e87158974bdeeb9e4e546b0991f0f4ba91
CalmXwang <whz643348767@gmail.com>
2024-11-06 16:20:30 -0800

    INT SORT
    Add integer sorting functionality and test

--- SortableArrayExercise/feature1/IntSorter.java
+++ SortableArrayExercise/feature1/IntSorter.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
 
-public class array {
+public class IntSorter {
 
     public static void sortIntegers(int[] array) {
-        Arrays.sort(array); 
+        Arrays.sort(array);
         System.out.println("Sorted Integer Array: " + Arrays.toString(array));
     }
 
     public static void main(String[] args) {
-        int[] intArray = {6,7,2,1,5};
+        int[] intArray = {5, 3, 8, 1, 2};
         System.out.println("Original Integer Array: " + Arrays.toString(intArray));
-        sortIntegers(intArray); 
+        sortIntegers(intArray);
     }
 }
--- SortableArrayExercise/feature1/IntSorterTest.java
+++ SortableArrayExercise/feature1/IntSorterTest.java
@@ -1,15 +1,14 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
 
-public class SortableArrayTest {
+public class IntSorterTest {
 
     @Test
     public void testSortIntegers() {
         int[] inputArray = {5, 3, 8, 1, 2};
         int[] expectedArray = {1, 2, 3, 5, 8};
         
-        // 调用排序方法
-        array.sortIntegers(inputArray);
+        IntSorter.sortIntegers(inputArray);
         
         assertArrayEquals(expectedArray, inputArray, "The integer array should be sorted in ascending order.");
     }
